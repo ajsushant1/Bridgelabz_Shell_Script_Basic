b39ee8780c1e7302750ee9ddb1c37c0ef7e56118
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-12 19:18:40 +0530

    MODIFIED: Proper indentation in program and removing extra space

--- ifelse/FlipCoin.sh
+++ ifelse/FlipCoin.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
 # TO GET RANDOM VALUE
-	randomCheck=$((RANDOM%2));
+randomCheck=$((RANDOM%2));
 
 # TO CHECK HEAD OR TAIL
-	if [ $randomCheck -eq 1 ]
-	then
-		echo "Head ";
-	else
-		echo "Tail";
-	fi
+if [ $randomCheck -eq 1 ]
+then
+	echo "Head ";
+else
+	echo "Tail";
+fi
