f9324d5976791305367a3269627f3f4718777f20
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-13 14:56:01 +0530

    MODIFIED: Indentation in code and variables name

--- for_loop/PrimeNumberInRange.sh
+++ for_loop/PrimeNumberInRange.sh
@@ -1,34 +1,34 @@
 #!/bin/bash -x
 
+#VARIABLE
+flag=0
+
 # READ THE RANGE OF NUMBER FROM USER
-	read -p "Enter starting number for range :" start
-	read -p "Enter ending number for range :" end
+read -p "Enter starting number for range :" start
+read -p "Enter ending number for range :" end
 
+# CONDITION TO CHECK IF START IS 1
+if [ $start -eq 1 ]
+then
+	start=$(($start+1))
+fi
 
-	if [ $start -eq 1 ]
-	then
-		start=$(($start+1))
-	fi
-
-# LOOP FOR RANGE
-for (( i=$start; i<=$end; i++ ))
+echo "Prime number between $start to $end are :"
+#LOOP FOR RANGE
+for (( count=$start; count<=$end; count++ ))
 do
 	flag=0;
-
-	# LOOP TO FIND PRIME NUMBER IN BETWEEN GIVEN RANGE
-		for (( j=2; j<=$(($i/2)); j++ ))
-		do
-
-			if [ $(($i%j)) -eq 0 ]
-			then
-					flag=1;
-					break;
-			fi
-		done
-
+# LOOP TO FIND PRIME NUMBER IN BETWEEN GIVEN RANGE
+	for (( divisor=2; divisor<=$(($count/2)); divisor++ ))
+	do
+		if [ $(($count%divisor)) -eq 0 ]
+		then
+			flag=1;
+			break;
+		fi
+	done
 	if [ $flag -eq 0 ]
 	then
-			echo $i
+		echo $count
 	fi
-
 done
