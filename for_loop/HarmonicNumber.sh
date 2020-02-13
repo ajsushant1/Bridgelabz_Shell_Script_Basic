dbeacbc86e3a78b7bcd84e8b61cb6cafb993b4c4
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-13 12:26:30 +0530

    MODIFIED: Indentation in code and variable name

--- for_loop/HarmonicNumber.sh
+++ for_loop/HarmonicNumber.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -x
 
 # VARIABLE
-
 harmonicValue=0
 
 # READ NUMBER FROM USER 
-	read -p "Enter a limit to compute harmonic value :" limit
+read -p "Enter a limit to compute harmonic value :" limit
 
 # TO COMPUTE HARMONIC NUMBER
-for ((i=1; i<=limit; i++ ))
+for ((denominator=1; denominator<=limit; denominator++ ))
 do
-	harmonicValue=$( echo "scale=2; $harmonicValue+((1/$i))" | bc )
+	harmonicValue=$( echo "scale=2; $harmonicValue+((1/$denominator))" | bc )
 done
+# PRINTING HARMONIC VALUE
 echo $harmonicValue
