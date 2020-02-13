e5b906e40eef30cad1123494e49a50c2a4d6f26e
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-13 09:35:26 +0530

    REFACTORED: Added new condition for user input

--- ifelse/LeapYearOrNot.sh
+++ ifelse/LeapYearOrNot.sh
@@ -1,12 +1,17 @@
 #!/bin/bash -x
 
 # READ YEAR FROM USER
-	read -p "Enter year :" year
+read -p "Enter year :" year
 
-#TO CHECK YAER IS LEAP YEAR OR NOT
+# TO CHECK YAER IS LEAP YEAR OR NOT
+if [ $year -gt 999 -a $year -le 9999 ]
+then
 	if [ $(($year%4)) -eq 0 -a $(($year%100)) -ne 0 -o $(($year%400)) -eq 0 ]
 	then
-			echo "$year is leap year"
+		echo "$year is leap year"
 	else
-			echo "$year is not leap year"
+		echo "$year is not leap year"
 	fi
+else
+	echo "Enter valid four digit year"
+fi
