6526814e8c427a50e4f389a78a308440a3d259ca
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-13 12:42:33 +0530

    MODIFIED: Indentation in code and printing message

--- for_loop/PowerOfTwo.sh
+++ for_loop/PowerOfTwo.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -x
 
 # VARIABLE
-	power=1
+power=1
 
 # READING COMMAND LINE ARGUMENT 
-	limit=$1
+limit=$1
 
 # CACULATING AND PRINTING TABLE OF POWER OF TWO 
-for(( count=0; count<=limit; count++ ))
+for(( count=1; count<=limit; count++ ))
 do
 	power=$((power*2))
-	echo "$power"
+	echo "Power of 2^$count is: $power"
 done
