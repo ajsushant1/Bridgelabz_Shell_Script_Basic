eb9f8779c7dc44fd4ced42f9942673909c26fc3b
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-13 18:02:39 +0530

    MODIFIED: Indentation in code

--- functions/PalindromeNumber.sh
+++ functions/PalindromeNumber.sh
@@ -9,15 +9,13 @@ read -p "Enter the number to check palindrome or not :" number
 # TO GET REVERSE NUMBER
 function isPalindrome(){
 local input=$1
-
 	while [ $input -ne 0 ]
 	do
-			remainder=$(($input%10))
-			sum=$(((($sum*10))+$remainder))
-			input=$(($input/10))
+		remainder=$(($input%10))
+		sum=$(((($sum*10))+$remainder))
+		input=$(($input/10))
 	done
 	echo $sum
-
 }
 
 # TO GET RESULT FROM FUNCTION
@@ -28,5 +26,5 @@ if [ $result -eq $number ]
 then
 	echo "$number is palindrome " 
 else
-		echo "Not a palindrome"
+	echo "Not a palindrome"
 fi
