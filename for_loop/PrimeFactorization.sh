df284e8967091e91ff8281f4caa468d36ac75f15
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-02-13 12:59:16 +0530

    MODIFIED: Indentation in code and variable name

--- for_loop/PrimeFactorization.sh
+++ for_loop/PrimeFactorization.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -x
 
 # READ NUMBER FROM  USER
-	read -p "Enter a number to find prime factorization :" number
+read -p "Enter a number to find prime factorization :" number
 
 # TO COMPUTE PRIME FACTORIZATION OF NUMBER
-for (( i=2; i*i<=$number; ))
+echo "Prime factor of $number are :"
+for (( factor=2; factor<=$number; ))
 do
-	if [ $((number%i)) -eq 0 ]
+	if [ $((number%factor)) -eq 0 ]
 	then
-			number=$(($number/i))
-			echo "$i"
+		number=$(($number/factor))
+		echo "$factor"
 	else
-		((i++))
+		((factor++))
 	fi
 done
